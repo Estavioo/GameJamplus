7961d5ef9ac95b5bbefe4bbcb16ea9ede7e4c9b4
Estavio <louie.estella2135@gmail.com>
2024-11-10 12:35:05 +0800

    TurretSystem

--- GJ+/Scenes/wolf_enemy.gd
+++ GJ+/Scenes/wolf_enemy.gd
@@ -1,31 +1,7 @@
-extends CharacterBody2D
+extends PathFollow2D
 
-# Reference to the AnimatedSprite2D node
-@onready var animated_sprite = $AnimatedSprite2D
+var health = 5
+var speed = 30
 
-# Variables to track previous and current positions
-var previous_position: Vector2
-var direction: Vector2
 
-# Called when the node enters the scene
-func _ready():
-	previous_position = position  # Set the initial previous position
 
-# Called every frame
-func _process(delta):
-	# Calculate movement direction
-	direction = position - previous_position
-	previous_position = position  # Update previous position
-
-	# Choose animation based on movement direction
-	if direction.length() > 0:
-		if abs(direction.x) > abs(direction.y):
-			if direction.x > 0:
-				animated_sprite.play("Right")
-			else:
-				animated_sprite.play("Left")
-		else:
-			if direction.y > 0:
-				animated_sprite.play("Down")
-			else:
-				animated_sprite.play("Up")
